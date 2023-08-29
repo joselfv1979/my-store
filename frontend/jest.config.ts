83f8979b3ef7b0ccbf2a6bec6eae3bce9de35b63
joselfv <joselfv.1979@gmail.com>
2023-08-29 16:11:26 +0200

    jest.config added

---
+++ frontend/jest.config.ts
@@ -0,0 +1,45 @@
+// module.exports = {
+//   preset: "ts-jest",
+//   moduleNameMapper: {
+//     // if your using tsconfig.paths thers is no harm in telling jest
+//     "@components/(.*)$": "<rootDir>/src/components/$1",
+//     "@/(.*)$": "<rootDir>/src/$1",
+
+//     // mocking assests and styling
+//     "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+//       "<rootDir>/tests/mocks/fileMock.ts",
+//     "^.+\\.(css|less|scss|sass)$": "<rootDir>/tests/mocks/styleMock.ts",
+//     /* mock models and services folder */
+//     "(assets|models|services)": "<rootDir>/tests/mocks/fileMock.ts",
+//   },
+//   // to obtain access to the matchers.
+//   setupFilesAfterEnv: ["./tests/setupTests.ts"],
+
+//   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+//   modulePaths: ["<rootDir>"],
+//   testEnvironment: "jsdom",
+// };
+
+import type { Config } from "@jest/types";
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+  },
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    // if your using tsconfig.paths thers is no harm in telling jest
+    // "@components/(.*)$": "<rootDir>/src/components/$1",
+    // "@/(.*)$": "<rootDir>/src/$1",
+
+    // mocking assests and styling
+    "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/tests/mocks/fileMock.ts",
+    "^.+\\.(css|less|scss|sass)$": "<rootDir>/tests/mocks/styleMock.ts",
+    /* mock models and services folder */
+   // "(assets|models|services)": "<rootDir>/tests/mocks/fileMock.ts",
+  },
+  setupFilesAfterEnv: ["./tests/setupTests.ts"],
+};
+export default config;
