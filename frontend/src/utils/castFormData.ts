e3b07f6d42f69062fa96ee9e308b7166db18e80c
joselfv <joselfv.1979@gmail.com>
2023-08-31 10:04:01 +0200

    add rating property

--- frontend/src/utils/castFormData.ts
+++ frontend/src/utils/castFormData.ts
@@ -1,15 +1,16 @@
 import { Product } from "../types/Product";
 
 export const castProductToFormData = (product: Product) => {
-  const { id, name, category, price, description, image, imagePath } = product;
-  const formData = new FormData();
+  const { id, name, category, price, description, rating, image, imagePath } = product;
+  const formData = new FormData();  
 
   if (id) formData.append("id", String(id));
+  if (rating) formData.append("rating", String(rating));
   formData.append("name", name);
   formData.append("category", category);
-  formData.append("price", price.toString());
-  formData.append("description", description.toString());
-  formData.append("image", image ? image : imagePath);
+  formData.append("price", String(price));
+  formData.append("description", description);
+  formData.append("image", image ?? imagePath);
 
   for (let [name, value] of formData) {
     console.log(`${name} = ${value}`); // key1 = value1, then key2 = value2
