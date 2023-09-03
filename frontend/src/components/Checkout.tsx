dc0f8541f04f56ac32dd5b8c42a901db19ebbbca
joselfv <joselfv.1979@gmail.com>
2023-09-03 18:04:57 +0200

    clear cart added

--- frontend/src/components/Checkout.tsx
+++ frontend/src/components/Checkout.tsx
@@ -1,34 +1,47 @@
 import React from "react";
 import styles from "./../scss/CartPage.module.scss";
-import { useAppSelector } from "../hooks/redux-hooks";
+import { useAppDispatch, useAppSelector } from "../hooks/redux-hooks";
 import { totalItems, totalPrice } from "../store/cart/cartSlice";
 import { useNavigate } from "react-router-dom";
 import { Button, Card, Stack } from "react-bootstrap";
+import { emptyCart } from "store/cart/cartActions";
 export const Checkout = () => {
   const items = useAppSelector(totalItems);
   const subtotal = Number(useAppSelector(totalPrice));
   const SHIPPING = 5;
 
+  const dispatch = useAppDispatch();
+
+  const clearCart = () => {
+    dispatch(emptyCart());
+  };
+
   const navigate = useNavigate();
-  
+
   return (
     <>
       <Card className={styles.checkoutCard}>
-      <Card.Header className={styles.checkoutTitle}>
-            <Card.Title>Checkout</Card.Title>
-          </Card.Header>
+        <Card.Header className={styles.checkoutTitle}>
+          <Card.Title>Checkout</Card.Title>
+        </Card.Header>
         <Card.Body>
           <Card.Title className={styles.checkoutConcept}>
-            Total Items: <span className={styles.checkoutQuantity}>{items} U</span>
+            Total Items:
+            <span className={styles.checkoutQuantity}>{items} U</span>
           </Card.Title>
           <Card.Title className={styles.checkoutConcept}>
-            Shipping: <span className={styles.checkoutQuantity}>{SHIPPING} €</span>
+            Shipping:
+            <span className={styles.checkoutQuantity}>{SHIPPING} €</span>
           </Card.Title>
           <Card.Title className={styles.checkoutConcept}>
-            Subtotal: <span className={styles.checkoutQuantity}>{subtotal} €</span>
+            Subtotal:
+            <span className={styles.checkoutQuantity}>{subtotal} €</span>
           </Card.Title>
           <Card.Title className={styles.checkoutConcept}>
-            Total invoice: <span className={styles.checkoutQuantity}>{subtotal + SHIPPING} €</span>
+            Total invoice:
+            <span className={styles.checkoutQuantity}>
+              {subtotal + SHIPPING} €
+            </span>
           </Card.Title>
         </Card.Body>
         <Card.Footer>
@@ -36,7 +49,11 @@ export const Checkout = () => {
             <Button variant="dark" className={styles.checkoutButton}>
               Checkout
             </Button>
-            <Button variant="danger" className={styles.checkoutButton}>
+            <Button
+              variant="danger"
+              className={styles.checkoutButton}
+              onClick={clearCart}
+            >
               Clear
             </Button>
           </Stack>
