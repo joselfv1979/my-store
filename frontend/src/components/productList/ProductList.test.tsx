6d4dab1da45f875313542f02dfd7c4d172780eec
joselfv <joselfv.1979@gmail.com>
2023-10-29 17:32:52 +0100

    product list test added

---
+++ frontend/src/components/productList/ProductList.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "../../tests/utils/test-utils";
+import ProductList from "./ProductList";
+import { type Product, type ProductState } from "../../types/Product";
+
+const productList: Product[] = [
+  {
+    id: "1",
+    name: "apple",
+    category: "food",
+    description: "",
+    imagePath: "",
+    price: 1.35,
+    rating: 3,
+  },
+];
+
+const initialProductState: ProductState = {
+  products: productList,
+  product: null,
+  status: 'success',
+};
+
+jest.mock("../../store/product/productActions", () => ({
+  fetchProducts: jest.fn(() => Promise.resolve(productList)),
+}));
+
+jest.mock("../../services/productService", () => ({
+  getProducts: jest.fn(() =>
+    Promise.resolve({ success: true, value: productList })
+  ),
+}));
+
+test("renders product list", async () => {
+  render(<ProductList />, {
+    preloadedState: {
+      product: initialProductState
+    }
+  });
+
+  const labelElement = screen.getByText(/Product list/i);
+  expect(labelElement).toBeInTheDocument();
+  expect(screen.getByText('apple')).toBeInTheDocument();
+});
