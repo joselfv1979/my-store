afdfd380d1e8360b6cfe36906b16a23e9d67d3a1
joselfv <joselfv.1979@gmail.com>
2023-10-29 17:26:44 +0100

    validateProduct modified

--- frontend/src/utils/validateProduct.ts
+++ frontend/src/utils/validateProduct.ts
@@ -5,9 +5,9 @@ export const validateProduct = (product: FormData): Result<FormData, string> => 
   const data = Object.fromEntries(product.entries());
   const { name, description, price } = data;  
 
-  if (name.length < 4) {
+  if (String(name).length < 4) {
     return { success: false, message: "Name must contains 3 characters at least" };
-  } else if (description.length < 3) {
+  } else if (String(description).length < 3) {
     return { success: false, message: "Description must contains 4 characters at least" };
   } else if (isNaN(Number(price))){
     return { success: false, message: "Number type is required" };
