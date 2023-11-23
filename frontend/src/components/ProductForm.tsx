98ab2ba77ab2983db8cbf8462dfe91327d343229
joselfv <joselfv.1979@gmail.com>
2023-11-23 09:40:30 +0100

    ProductForm style modified

--- frontend/src/components/ProductForm.tsx
+++ frontend/src/components/ProductForm.tsx
@@ -1,14 +1,16 @@
-import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../hooks/redux-hooks";
-import styles from "../assets/scss/ProductFormPage.module.scss";
 import { initialProduct, Product } from "../types/Product";
 import { castProductToFormData } from "../utils/castFormData";
 import { categories } from "../data/ConstantUtils";
+import Form from "react-bootstrap/Form";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import ImageUpload from "./imageUpload/ImageUpload";
 
 type Props = {
   saveProduct: (data: FormData) => Promise<void>;
-  editing?: boolean; // if true, show the edit form, else show the create form.
+  editing?: boolean; // if true, show the edit form, otherwise show the create form.
 };
 
 type InputType = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
@@ -20,122 +22,138 @@ const ProductForm = ({ saveProduct, editing = false }: Props) => {
 
   const [productData, setProductData] = useState<Product>(currentProduct);
 
+  const formTitle = editing ? "Edit Product" : "New Product";
+
+  const navigate = useNavigate();
+
   const handleInputEvent = (event: ChangeEvent<InputType>) => {
     setProductData({ ...productData, [event.target.name]: event.target.value });
   };
 
-  const handleImage = (event: ChangeEvent<HTMLInputElement>) => {
-    event.target.files &&
-    setProductData({ ...productData, image: event.target.files[0] });
-  };
-
-  const fileInput = useRef<HTMLInputElement>(null);
-
-  const imagePath =
-  productData.imagePath.length > 0
-      ? `${process.env.REACT_APP_API_IMAGES}/${productData.imagePath}`
-      : null;
-      
-  const url = productData.image ? URL.createObjectURL(productData.image) : imagePath;
-
   const sendDataProduct = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    const productform = castProductToFormData(productData);    
+    const productform = castProductToFormData(productData);
     await saveProduct(productform);
   };
 
-  const navigate = useNavigate();
-
   return (
-    <form className={styles.form} onSubmit={sendDataProduct}>
-      {editing ? <h2>Edit Product</h2> : <h2>New Product</h2>}
+      <Card className="d-flex w-50 mt-4">
+        <Card.Header as="h4" className="text-center">{formTitle}</Card.Header>
 
-      <fieldset>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          autoComplete="off"
-          required
-          onChange={handleInputEvent}
-          value={productData.name}
-        />
-      </fieldset>
+        <Card.Body className="mt-2">
+          <Form className="p-1" onSubmit={sendDataProduct}>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formPlaintextEmail"
+            >
+              <Form.Label column sm="3">
+                Name
+              </Form.Label>
+              <Col sm="8">
+                <Form.Control
+                  type="text"
+                  name="name"
+                  placeholder="Product name"
+                  required
+                  size="sm"
+                  onChange={handleInputEvent}
+                  defaultValue={productData.name}
+                />
+              </Col>
+            </Form.Group>
 
-      <fieldset>
-        <label htmlFor="category">Category</label>
-        <select
-          name="category"
-          id="category"
-          required
-          onChange={handleInputEvent}
-          value={productData.category}
-        >
-          {categories.map((category) => (
-            <option key={category.id} value={category.value}>
-              {category.label}
-            </option>
-          ))}
-        </select>
-      </fieldset>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formPlaintextEmail"
+            >
+              <Form.Label column sm="3">
+                Category
+              </Form.Label>
+              <Col sm="8">
+              <Form.Select
+                aria-label="Default select example"
+                onChange={handleInputEvent}
+                name="category"
+                defaultValue={productData.category}
+              >
+                {categories.map((category) => (
+                  <option key={category.id} value={category.value}>
+                    {category.label}
+                  </option>
+                ))}
+              </Form.Select>
+              </Col>
+            </Form.Group>
 
-      <fieldset>
-        <label htmlFor="price">Price</label>
-        <input
-          type="number"
-          name="price"
-          id="price"
-          step="0.01"
-          required
-          onChange={handleInputEvent}
-          value={productData.price}
-        />
-      </fieldset>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formPlaintextEmail"
+            >
+              <Form.Label column sm="3">
+                Price
+              </Form.Label>
+              <Col sm="8">
+                <Form.Control
+                  type="number"
+                  name="price"
+                  placeholder="name_example"
+                  required
+                  size="sm"
+                  onChange={handleInputEvent}
+                  defaultValue={productData.price}
+                />
+              </Col>
+            </Form.Group>
 
-      <fieldset className={styles.fullColumn}>
-        <label htmlFor="description">Description</label>
-        <textarea
-          name="description"
-          id="description"
-          required
-          onChange={handleInputEvent}
-          value={productData.description}
-        />
-      </fieldset>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formPlaintextEmail"
+            >
+              <Form.Label column sm="3">
+                Description
+              </Form.Label>
+              <Col sm="8">
+                <Form.Control
+                as="textarea" rows={3}
+                  type="textarea"
+                  name="description"
+                  placeholder="Leave a comment here"
+                  required
+                  size="sm"
+                  onChange={handleInputEvent}
+                  defaultValue={productData.description}
+                />
+              </Col>
+            </Form.Group>
 
-      <fieldset className={styles.imageField}>
-        <div className={styles.loadContainer}>
-          <label htmlFor="image">Image</label>
-          <input
-            className={styles.inputFile}
-            type="file"
-            ref={fileInput}
-            name="image"
-            id="image"
-            accept=".jpg,.jpeg,.png"
-            onChange={handleImage}
-          />
-        </div>
-        <div className={styles.imageContainer}>
-          {url ? (
-            <img src={url} alt="File Preview" />
-          ) : (
-            <p>no image selected</p>
-          )}
-        </div>
-      </fieldset>
+            <ImageUpload
+              productData={productData}
+              setProductData={setProductData}
+            />
 
-      <div className={styles.buttonsContainer}>
-        <button>Submit</button>
-
-        <button onClick={() => navigate("/")} className={styles.cancel}>
-          Cancel
-        </button>
-      </div>
-    </form>
+            <div className="d-flex justify-content-center">
+              <Button variant="success" size="lg" className="mx-5" type="submit">
+                Submit
+              </Button>
+              <Button variant="secondary" size="lg" className="mx-5" onClick={() => navigate("/")}>
+                Cancel
+              </Button>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
   );
 };
 
 export default ProductForm;
+
+
+
+
+
+
+
