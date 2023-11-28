e90e9b6662d413af500c881a6f6b070584698080
joselfv <joselfv.1979@gmail.com>
2023-11-28 21:39:43 +0100

    change ProductCard style

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,13 +1,8 @@
-import styles from "../assets/scss/ProductListPage.module.scss";
 import { Product } from "../types/Product";
-import Card from "react-bootstrap/Card";
-import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../hooks/redux-hooks";
 import AdminButtons from "./adminButtons/AdminButtons";
 import { isAdmin } from "../store/user/userSlice";
 import UserButtons from "./UserButtons";
-import StarRating from "./StarRating";
-import { Stack } from "react-bootstrap";
 
 type Props = {
   product: Product;
@@ -16,37 +11,24 @@ type Props = {
 const ProductCard = ({ product }: Props) => {
   const admin = useAppSelector(isAdmin);
 
-  const navigate = useNavigate();
-
-  const handleOnClick = () => {
-    navigate(`/product-detail/${product.id}`);
-  };
-
   const image = `${process.env.REACT_APP_API_IMAGES}/${product.imagePath}`;
 
   return (
-    <Card className={styles.card}>
-      <Stack onClick={handleOnClick} className={styles.productData}>
-        <Card.Img
-          variant="top"
-          className={styles.cardImg}
-          src={image}
-          alt={product.name}
-        />
-        <Card.Body>
-          <Card.Title>{product.name}</Card.Title>
-          <Card.Title>Price: {product.price} €</Card.Title>
-          <StarRating stars={product.rating} />
-        </Card.Body>
-      </Stack>
-      <Card.Footer className={styles.buttonGroup}>
-        {admin ? (
-          <AdminButtons id={product.id} />
-        ) : (
-          <UserButtons product={product} style={styles.userButton} />
-        )}
-      </Card.Footer>
-    </Card>
+    <div className="card">
+      <img src={image} className="card-img-top" alt={product.name} />
+      <div className="card-body">
+        <h5 className="card-title">{product.name}</h5>
+        <p className="card-text">Price: {product.price} €</p>
+        <p className="card-text">{product.description}</p>
+        <div className="d-flex justify-content-around">
+          {admin ? (
+            <AdminButtons id={product.id} />
+          ) : (
+            <UserButtons product={product} />
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
