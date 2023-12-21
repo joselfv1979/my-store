24c479a47272ce681f2640a5d7eb6ec4d2f217f8
joselfv <joselfv.1979@gmail.com>
2023-12-21 07:20:25 +0100

    create RequestPasswordResetModal

---
+++ frontend/src/components/requestPasswordResetModal/RequestPasswordResetModal.tsx
@@ -0,0 +1,60 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { requestPasswordReset } from "services/authService";
+
+type Props = {
+  showPasswordResetModal: boolean;
+  setShowPasswordResetModal: (show: boolean) => void;
+};
+
+const RequestPasswordResetModal = ({
+  showPasswordResetModal,
+  setShowPasswordResetModal,
+}: Props) => {
+  const [value, setValue] = useState({ email: "" });
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue({ email: event.target.value });
+  };
+
+  const sendRequest = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await requestPasswordReset(value);
+  };
+
+  const handleClose = () => setShowPasswordResetModal(false);
+
+  return (
+    <Modal
+      centered
+      backdrop="static"
+      show={showPasswordResetModal}
+      onHide={handleClose}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>User password reset</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={sendRequest}>
+          <h6>Request password reset email</h6>
+          <Form.Group className="my-3" controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              name="email"
+              autoFocus
+              required
+              placeholder="Your account email"
+              onChange={onChange}
+            />
+          </Form.Group>
+          <Button variant="primary" className="mt-3 w-100" type="submit">
+            Request password reset
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default RequestPasswordResetModal;
