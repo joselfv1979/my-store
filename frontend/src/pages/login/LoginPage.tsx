251297f372346bd1340019b45879333c1dbd1caa
joselfv <joselfv.1979@gmail.com>
2023-12-21 07:26:25 +0100

    add googleLogin and requestPasswordResetModal components

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -2,24 +2,32 @@ import { Navigate } from "react-router-dom";
 import { AppMessage, AppWaiting } from "../../components/appStatus/AppStatus";
 import LoginForm from "../../components/LoginForm";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux-hooks";
-import { cancelUserMessage, login } from "../../store/user/userActions";
 import { Message, Status } from "../../types/Message";
 import { AuthRequest } from "../../types/User";
-import { fetchProducts } from "../../store/product/productActions";
+import { useState } from "react";
+import RequestPasswordResetModal from "components/requestPasswordResetModal/RequestPasswordResetModal";
+import GoogleLoginComponent from "components/googleLogin/GoogleLogin";
+import { cancelUserMessage, login, loginGoogle } from "store/user/userActions";
+import { CredentialResponse } from "@react-oauth/google";
 
 const LoginPage = () => {
   const dispatch = useAppDispatch();
 
   const { authUser, error, status } = useAppSelector((state) => state.user);
+  const [showPasswordResetModal, setShowPasswordResetModal] =
+    useState<boolean>(false);
 
   const note: Message = {
     type: Status.DANGER,
     text: error,
   };
 
-  const loginUser = (user: AuthRequest) => {
+  const loginUser = async (user: AuthRequest) => {
     dispatch(login(user));
-    dispatch(fetchProducts(""));
+  };
+
+  const loginGoogleUser = async (credential: CredentialResponse) => {    
+    dispatch(loginGoogle(credential));
   };
 
   const cancelMessage = () => {
@@ -31,7 +39,21 @@ const LoginPage = () => {
   return (
     <>
       {note.text && <AppMessage note={note} cancelMessage={cancelMessage} />}
-      {status === 'loading' ? <AppWaiting /> : <LoginForm loginUser={loginUser} />}
+      {status === "loading" ? (
+        <AppWaiting />
+      ) : (
+        <>
+          <LoginForm
+            loginUser={loginUser}
+            setShowPasswordResetModal={setShowPasswordResetModal}
+          />
+          <GoogleLoginComponent loginGoogleUser={loginGoogleUser} />
+        </>
+      )}
+      <RequestPasswordResetModal
+        showPasswordResetModal={showPasswordResetModal}
+        setShowPasswordResetModal={setShowPasswordResetModal}
+      />
     </>
   );
 };
