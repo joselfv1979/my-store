83f419282d52add00f088674692244c016511f03
joselfv <joselfv.1979@gmail.com>
2023-12-21 07:29:46 +0100

    create authService file

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,64 @@
+import { CredentialResponse } from "@react-oauth/google";
+import axios from "axios";
+import { Result } from "types/Result";
+import { AuthRequest, AuthUser } from "types/User";
+import { handleError } from "utils/handleError";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_AUTH,
+});
+
+/**
+ * Returns user's login data
+ * @param {AuthRequest} credentials - {username, password}.
+ * @returns {Result<AuthResponse, string>} User's data or an error messsage
+ */
+export const loginUser = async (
+  credentials: AuthRequest
+): Promise<Result<AuthUser, string>> => {
+  try {
+    const { data } = await api.post("/sign-in", credentials);
+
+    return { success: true, value: data };
+  } catch (error) {
+    return { success: false, message: handleError(error) };
+  }
+};
+
+export const googleLogin = async (
+  credential: CredentialResponse
+): Promise<Result<AuthUser, string>> => {
+  try {
+    const { data } = await api.post("/google-auth", credential);
+
+    return { success: true, value: data };
+  } catch (error) {
+    return { success: false, message: handleError(error) };
+  }
+};
+
+export const requestPasswordReset = async (body: { email: string }) => {
+  try {
+    await api.post("/request-password-reset", body);
+  } catch (error) {
+    console.log({ error });
+    return { success: false, message: handleError(error) };
+  }
+};
+
+export const passwordReset = async (
+  body: { password: string },
+  token: string
+) => {
+  try {
+    await api.put("/password-reset", body, {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return { success: false, message: handleError(error) };
+  }
+};
--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,44 +1,12 @@
 import axios from "axios";
 import { Result } from "../types/Result";
-import { AuthRequest, AuthResponse, User } from "../types/User";
+import { User } from "../types/User";
 import { handleError } from "../utils/handleError";
 import { getHeaders } from "../utils/authHeader";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_USERS,
 });
-
-/**
- * Returns user's login data
- * @param {AuthRequest} credentials - {username, password}.
- * @returns {Result<AuthResponse, string>} User's data or an error messsage
- */
-export const loginUser = async (
-  credentials: AuthRequest
-): Promise<Result<AuthResponse, string>> => {
-  try {
-    const { data } = await api.post("/sign-in", credentials);
-    console.log({ data });
-
-    return { success: true, value: data };
-  } catch (error) {
-    return { success: false, message: handleError(error) };
-  }
-};
-
-export const googleLogin = async ( token: { token: string} ): Promise<Result<AuthResponse, string>> => {
-  
-  console.log('token-g', token);
-  
-  try {
-    const { data } = await api.post('/auth/google', token);
-    console.log({ data });
-
-    return { success: true, value: data };
-  } catch (error) {
-    return { success: false, message: handleError(error) };
-  }
-};
 
 /**
  * Returns all users
@@ -62,6 +30,9 @@ export const getUsers = async (): Promise<Result<User[], string>> => {
  */
 export const getUser = async (id: string): Promise<Result<User, string>> => {
   try {
+    console.log("id", id);
+    console.log("heders", getHeaders());
+
     const { data } = await api.get(`/${id}`, {
       headers: getHeaders(),
     });
